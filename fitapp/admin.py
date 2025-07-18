46156d682543e288355eeaed03c71ab9bd3242fa
saijayanthsri <srisaijayanth@gmail.com>
2025-07-18 09:49:54 +0530

    Initial commit

---
+++ fitapp/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from fitapp.models import Contact,MembershipPlan,Enrollment,Trainer,Photos,Attendance
+
+
+
+# Register your models here.
+admin.site.register(Contact)
+admin.site.register(MembershipPlan)
+admin.site.register(Enrollment)
+admin.site.register(Trainer)
+admin.site.register(Photos)
+admin.site.register(Attendance)
+
+
+
+
+
+
+# Register your models here.
---
+++ fitapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from fitapp import views
+
+urlpatterns = [
+    path('',views.Home,name="Home"),
+    path('signup',views.signup,name="signup"),
+    path('login',views.handlelogin,name="handlelogin"),
+    path('logout',views.handlelogout,name="handlelogout"),
+    path('contact',views.contact,name="contact"),
+    path('join',views.enroll,name="enroll"),
+    path('profile',views.profile,name="profile"),
+    path('Photos',views.Photos,name="Photos"),
+    path('attendance',views.attendance,name="attendance"),
+
+]
